39a0b71ae6fd417c0f40dfc12520b6fb9a19c73f
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2023-11-10 17:45:10 -0400

    Creating product schema

---
+++ src/data/mongodb/models/product.model.ts
@@ -0,0 +1,50 @@
+import mongoose , {Schema} from "mongoose";
+
+const productSchema = new Schema({
+
+    name:{
+        type:String,
+        required:[true,'Name is required']
+    },
+
+    description:{
+        type: String,
+        required: [true, 'Description is required']
+    },
+    available:{
+        type: Boolean,
+        default:true,
+    },
+
+    img:{
+        type:String
+    },
+    status:{
+        type:Boolean,
+        default:true
+    },
+
+    category:{
+        type: Schema.Types.ObjectId,
+        ref:'Category',
+        required:true,
+    },
+    user:{
+        type: Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    }
+
+
+});
+
+
+productSchema.methods.toJSON = function(){
+    const {__v,estado,...data} = this.toObject();
+    
+    return data;
+}
+
+export const ProductMOdel = mongoose.model('Product',productSchema);
+
+
